4c97a77e2c9b023fbabb8a22ea43eb5420bd52e7
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-10-14 19:58:52 +0300

    Fix link visibility

--- templates/listing/view/page/listing-claim-link.php
+++ templates/listing/view/page/listing-claim-link.php
@@ -1,6 +1,9 @@
 <?php
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-// todo.
-?>
-<a href="#<?php if ( is_user_logged_in() ) : ?>listing_claim<?php else : ?>user_login<?php endif; ?>_modal" class="hp-listing__action hp-link"><i class="hp-icon fas fa-shield-alt"></i><span><?php esc_html_e( 'Claim Listing', 'hivepress-claim-listings' ); ?></span></a>
+
+if ( ! is_user_logged_in() || get_current_user_id() !== get_the_author_meta( 'ID' ) ) :
+	?>
+	<a href="#<?php if ( is_user_logged_in() ) : ?>listing_claim<?php else : ?>user_login<?php endif; ?>_modal" class="hp-listing__action hp-link"><i class="hp-icon fas fa-shield-alt"></i><span><?php esc_html_e( 'Claim Listing', 'hivepress-claim-listings' ); ?></span></a>
+	<?php
+endif;
