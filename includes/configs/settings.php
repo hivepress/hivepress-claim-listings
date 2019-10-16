42f14c2c707609825b0ade81c8754e1b08250f11
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-10-16 23:50:58 +0300

    Add settings

--- includes/configs/settings.php
+++ includes/configs/settings.php
@@ -17,7 +17,24 @@ return [
 				'title'  => esc_html__( 'Claiming', 'hivepress-claim-listings' ),
 				'order'  => 30,
 
-				'fields' => [],
+				'fields' => [
+					'product_claim_todo'           => [
+						'label'       => esc_html__( 'WooCommerce Product', 'hivepress-claim-listings' ),
+						'description' => esc_html__( 'Choose a WooCommerce product that must be purchased in order to submit a claim.', 'hivepress-claim-listings' ),
+						'type'        => 'select',
+						'options'     => 'posts',
+						'post_type'   => 'product',
+						'order'       => 10,
+					],
+
+					'claim_enable_moderation_todo' => [
+						'label'   => esc_html__( 'Moderation', 'hivepress-claim-listings' ),
+						'caption' => esc_html__( 'Manually approve new claims', 'hivepress-claim-listings' ),
+						'type'    => 'checkbox',
+						'default' => true,
+						'order'   => 20,
+					],
+				],
 			],
 		],
 	],
