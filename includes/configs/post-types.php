cfd2f1355a680971b2613d357c317666c5ef0423
Ihor Ahnianikov <ahnianikov@gmail.com>
2019-11-22 21:51:13 +0200

    Fix translation

--- includes/configs/post-types.php
+++ includes/configs/post-types.php
@@ -20,14 +20,14 @@ return [
 		'labels'       => [
 			'name'               => esc_html__( 'Claims', 'hivepress-claim-listings' ),
 			'singular_name'      => esc_html__( 'Claim', 'hivepress-claim-listings' ),
+			'add_new'            => esc_html_x( 'Add New', 'listing claim', 'hivepress-claim-listings' ),
 			'add_new_item'       => esc_html__( 'Add Claim', 'hivepress-claim-listings' ),
 			'edit_item'          => esc_html__( 'Edit Claim', 'hivepress-claim-listings' ),
 			'new_item'           => esc_html__( 'Add Claim', 'hivepress-claim-listings' ),
-			'view_item'          => esc_html__( 'View Claim', 'hivepress-claim-listings' ),
 			'all_items'          => esc_html__( 'Claims', 'hivepress-claim-listings' ),
 			'search_items'       => esc_html__( 'Search Claims', 'hivepress-claim-listings' ),
-			'not_found'          => esc_html__( 'No Claims Found', 'hivepress-claim-listings' ),
-			'not_found_in_trash' => esc_html__( 'No Claims Found', 'hivepress-claim-listings' ),
+			'not_found'          => esc_html__( 'No claims found.', 'hivepress-claim-listings' ),
+			'not_found_in_trash' => esc_html__( 'No claims found.', 'hivepress-claim-listings' ),
 		],
 	],
 ];
