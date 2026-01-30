61d69fd74e215d1ba1adc2bfd769de87faab12c0
HivePress <43493187+hivepress@users.noreply.github.com>
2026-01-30 16:22:25 +0100

    Minor fixes

--- hivepress-claim-listings.php
+++ hivepress-claim-listings.php
@@ -17,7 +17,7 @@ defined( 'ABSPATH' ) || exit;
 // Register extension directory.
 add_filter(
 	'hivepress/v1/extensions',
-	function ( $extensions ) {
+	function( $extensions ) {
 		$extensions[] = __DIR__;
 
 		return $extensions;
